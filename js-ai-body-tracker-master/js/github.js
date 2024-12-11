05a269124ed116e1a8581aff4c9f5cb0c8fcc087
My Macro Coach <jordan.c.horowitz@gmail.com>
2024-12-11 10:34:55 -0500

    Changed the stuff

--- js-ai-body-tracker-master/js/github.js
+++ js-ai-body-tracker-master/js/github.js
@@ -11,7 +11,7 @@ async function fetchGitHubInfo() {
         // Fetch all commits and count 'committer' occurrences
         const commitsResponse = await fetch(`https://api.github.com/repos/${owner}/${repo}/commits`);
         const commitsData = await commitsResponse.json();
-        const commitsText = JSON.stringify(commitsData);
+        const commitsText = JSON.stringify(commitsData);    
         const committerCount = (commitsText.match(/committer/g) || []).length;
         const totalCount = committerCount + 2;
         
