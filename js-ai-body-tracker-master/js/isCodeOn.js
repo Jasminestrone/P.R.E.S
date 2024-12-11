c4a12b89428ec30cdaa802e53a8796cb5109d866
StarBlazed <bradleystone4531@gmail.com>
2024-12-11 10:24:07 -0500

    Small notation and commit test for website

--- js-ai-body-tracker-master/js/isCodeOn.js
+++ js-ai-body-tracker-master/js/isCodeOn.js
@@ -4,11 +4,13 @@ const codeRunner = document.getElementById("codeRunner");
 
 function buttonSwap() {
     if (isRunning) {
+        // Swap to red button
         codeRunner.removeEventListener("click",startCode);
         codeRunner.addEventListener("click", stopCode);
         codeRunner.setAttribute("class", "btn btn-danger");
         codeRunner.textContent = 'Stop Presentation';
     } else if (!isRunning) {
+        // Swap to green button
         codeRunner.removeEventListener("click",stopCode);
         codeRunner.addEventListener("click", startCode);
         codeRunner.setAttribute("class", "btn btn-success");
